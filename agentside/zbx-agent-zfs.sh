58baa62924316de7655d6abc9ae1c6dabf8e9bbd
thorro <boris@splet.net>
2017-10-20 13:59:14 +0200

    fix awk

--- agentside/zbx-agent-zfs.sh
+++ agentside/zbx-agent-zfs.sh
@@ -25,14 +25,11 @@ UserParameter=zpool.rw.stat[*],cat /proc/spl/kstat/zfs/$1/io | tail -n +3 | awk 
 # Unit in %
 UserParameter=zpool.alloc.stat[*],sudo zpool list -H -o cap $1 | tr -d "%"
 # Unit in Kbytes, not cumulative
-#UserParameter=zpool.size.stat[*],df -l | grep -w "$1 " | awk '{print $$2}'
-UserParameter=zpool.size.stat[*],sudo zpool get -H -p size $1 | awk '{print $3}'
+UserParameter=zpool.size.stat[*],sudo zpool get -H -p size $1 | awk '{print $$3}'
 # Unit in Kbytes, not cumulative
-#UserParameter=zpool.used.stat[*],df -l | grep -w "$1 " | awk '{print $$3}'
-UserParameter=zpool.used.stat[*],sudo zpool get -H -p allocated $1 | awk '{print $3}'
+UserParameter=zpool.used.stat[*],sudo zpool get -H -p allocated $1 | awk '{print $$3}'
 # Unit in Kbytes, not cumulative
-#UserParameter=zpool.free.stat[*],df -l | grep -w "$1 " | awk '{print $$4}'
-UserParameter=zpool.free.stat[*],sudo zpool get -H -p free $1 | awk '{print $3}'
+UserParameter=zpool.free.stat[*],sudo zpool get -H -p free $1 | awk '{print $$3}'
 
 # DATASETS
 UserParameter=zsets.discover,/bin/discover-zfsdataset.sh
